810ee87d5fae779cc5cde8652bacf175a258a2c6
Jabez <jabezli1990@gmail.com>
2023-05-05 22:33:12 -0700

    able to get api response/data now

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
+const cityEl = document.querySelector(".search");
+const search = document.querySelector(".searchForm");
+
 //api f2e26748fb648875d4055b62c1e10d37
 
 const apiKey = "f2e26748fb648875d4055b62c1e10d37";
 
-let city;
+let city = [];
 
-let queryUrl =
-  "http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+const searchHandler = async (event) => {
+  event.preventDefault();
+  city = cityEl.value;
+
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+  const data = await response.json();
+  console.log(data);
+};
+
+search.addEventListener("submit", searchHandler);
