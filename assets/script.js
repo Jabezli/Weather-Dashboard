4e0e0bb9af7a56a3c7867023a6f94da8ac3840cd
Jabez <jabezli1990@gmail.com>
2023-05-05 21:47:34 -0700

    html main frame done

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
 //api f2e26748fb648875d4055b62c1e10d37
+
+const apiKey = "f2e26748fb648875d4055b62c1e10d37";
+
+let city;
+
+let queryUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
